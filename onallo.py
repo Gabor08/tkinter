b8435a5becc4743dd08dcee3fbf8d0da1d2eadba
Gabor08 <gavlikgabor@turr.hu>
2022-03-21 12:53:49 +0100

    1. mező kész

--- onallo.py
+++ onallo.py
@@ -1,4 +1,8 @@
 from tkinter import *
 ablak=tk()
-
+mezo1=Entry(ablak)
+mezo1.grid(row=0, column=2)
+mezo1felirat=Label(ablak, text="Első mező")
+mezo1felirat.grid(row=0, column=1)
+mezo2=Entry(ablak)
 ablak.mainloop
