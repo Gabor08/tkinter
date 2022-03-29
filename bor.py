16c2e78bec757ec89ffc0f5188bc0212cb449d4f
Gabor08 <gavlikgabor@turr.hu>
2022-03-29 11:31:06 +0200

    hordó térfogata, vizsgálata annak hogy belefér-e a bor kész

--- bor.py
+++ bor.py
@@ -1,5 +1,16 @@
 from tkinter import *
+import math
 foablak=Tk()
+def szamitasok():
+    negyedikmezo.delete(0, END)
+    sugar=int(masodikmezo.get())
+    magas=int(harmadikmezo.get())
+    terf=math.pi*sugar**2*magas
+    liter=terf/1000
+    bor=int(elsomezo.get())
+    otodikmezo.insert(0, "{:.0f}".format(liter)+" l")
+    if bor>liter:
+        negyedikmezo.insert(0, "Túl sok a bor.")
 elsomezo=Entry(foablak)
 elsomezo.grid(row=0, column=1)
 elsomezofelirat=Label(foablak, text="Bor mennyisége (l): ")
@@ -12,4 +23,10 @@ harmadikmezo=Entry(foablak)
 harmadikmezo.grid(row=2, column=1)
 harmadikmezofelirat=Label(foablak, text="Hordó magassága (cm): ")
 harmadikmezofelirat.grid(row=2, column=0)
+gomb=Button(foablak, text="Kiszámít", command=szamitasok)
+gomb.grid(row=3, column=1)
+negyedikmezo=Entry(foablak)
+negyedikmezo.grid(row=5, column=1)
+otodikmezo=Entry(foablak)
+otodikmezo.grid(row=4, column=1)
 foablak.mainloop()
