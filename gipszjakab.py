ea701cf82b8d3400a1e9a08ce16d85a459374fc2
Gabor08 <gavlikgabor@turr.hu>
2022-04-06 11:14:29 +0200

    minta (Gipsz Jakab) kész

---
+++ gipszjakab.py
@@ -0,0 +1,14 @@
+from tkinter import *
+foablak=Tk()
+def ujablak():
+    ablak=Toplevel(foablak)
+    uzenet=Message(ablak, text="Készítette: Gipsz Jakab\nPiripócs\n2009.06.04", width=300)
+    gomb2=Button(ablak, text="Kilépés", command=ablak.destroy)
+    uzenet.pack()
+    gomb2.pack()
+    ablabk.mainloop()
+szoveg=Label(foablak, text="Kattints a gombra!")
+szoveg.pack()
+gomb1=Button(foablak, text="Névjegy", command=ujablak)
+gomb1.pack()
+foablak.mainloop()
