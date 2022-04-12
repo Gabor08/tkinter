9133f350bb9bc140a5bdcfbfcc26c84aa5e927fd
Gabor08 <gavlikgabor@turr.hu>
2022-04-12 11:31:00 +0200

    henger a menu1-hez

--- menu1.py
+++ menu1.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 foablak=Tk()
 foablak.minsize(width=300, height=100)
 def nevjegy():
@@ -16,7 +17,7 @@ def felszin():
         mezo.delete(0, END)
         felszin=2*(a*b+b*c+c*a)
         if a>0 and b>0 and c>0:
-            mezo.insert(0, str(felszin))
+            mezo.insert(0, int(felszin))
         else:
             mezo.insert(0, "Nem lehet mínusz vagy 0 az éle.")
     ablak=Toplevel(foablak)
@@ -46,7 +47,7 @@ def terfogat():
         terfogat=a*b*c
         mezo.delete(0, END)
         if a>0 and b>0 and c>0:
-            mezo.insert(0, str(terfogat))
+            mezo.insert(0, int(terfogat))
         else:
             mezo.insert(0, "Nem lehet mínusz vagy 0 az éle.")
     ablak=Toplevel(foablak)
@@ -68,8 +69,58 @@ def terfogat():
     mezo.grid(row=4, column=1)
     mezofelirat=Label(ablak, text="Eredmény:")
     mezofelirat.grid(row=4, column=0)
+def hengerterfogat():
+    def szamitas():
+        sugar=int(amezo.get())
+        magassag=int(bmezo.get())
+        terf=sugar**2*math.pi*magassag
+        mezo.delete(0, END)
+        if sugar>0 and magassag>0:
+            mezo.insert(0, "{:.2f}".format(int(terf)))
+        else:
+            mezo.insert(0, "Nem lehet mínusz vagy 0 sem a magasság, sem a sugár.")
+    ablak=Toplevel(foablak)
+    amezo=Entry(ablak)
+    amezo.grid(row=0, column=1)
+    amezoszoveg=Label(ablak, text="Sugár:")
+    amezoszoveg.grid(row=0, column=0)
+    bmezo=Entry(ablak)
+    bmezo.grid(row=1, column=1)
+    bmezoszoveg=Label(ablak, text="Magasság:")
+    bmezoszoveg.grid(row=1, column=0)
+    gomb1=Button(ablak, text="Számítás", command=szamitas)
+    gomb1.grid(row=2, column=1)
+    mezo=Entry(ablak)
+    mezo.grid(row=3, column=1)
+    mezofelirat=Label(ablak, text="Eredmény:")
+    mezofelirat.grid(row=3, column=0)
+def hengerfelszin():
+    def szamitas():
+        sugar=int(amezo.get())
+        magassag=int(bmezo.get())
+        felszin=(2*sugar**2*math.pi)+(2*sugar*math.pi*magassag)
+        mezo.delete(0, END)
+        if sugar>0 and magassag>0:
+            mezo.insert(0, "{:.2f}".format(int(felszin)))
+        else:
+            mezo.insert(0, "Nem lehet mínusz vagy 0 sem a magasság, sem a sugár.")
+    ablak=Toplevel(foablak)
+    amezo=Entry(ablak)
+    amezo.grid(row=0, column=1)
+    amezoszoveg=Label(ablak, text="Sugár:")
+    amezoszoveg.grid(row=0, column=0)
+    bmezo=Entry(ablak)
+    bmezo.grid(row=1, column=1)
+    bmezoszoveg=Label(ablak, text="Magasság:")
+    bmezoszoveg.grid(row=1, column=0)
+    gomb1=Button(ablak, text="Számítás", command=szamitas)
+    gomb1.grid(row=2, column=1)
+    mezo=Entry(ablak)
+    mezo.grid(row=3, column=1)
+    mezofelirat=Label(ablak, text="Eredmény:")
+    mezofelirat.grid(row=3, column=0)
 menusor=Frame(foablak)
-menusor.pack()
+menusor.pack(side=TOP, fill=X)
 menu1=Menubutton(menusor, text="Fájl")
 menu1.pack(side=LEFT)
 fajl=Menu(menu1)
@@ -82,4 +133,10 @@ teglatest=Menu(menu2)
 teglatest.add_command(label="Felszín", command=felszin)
 teglatest.add_command(label="Térfogat", command=terfogat)
 menu2.config(menu=teglatest)
+menu3=Menubutton(menusor, text="Henger")
+menu3.pack(side=LEFT)
+henger=Menu(menu3)
+henger.add_command(label="Felszín", command=hengerfelszin)
+henger.add_command(label="Térfogat", command=hengerterfogat)
+menu3.config(menu=henger)
 foablak.mainloop()
